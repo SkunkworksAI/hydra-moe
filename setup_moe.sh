bcd8d1b3ecdfe2a51939580a94ee2c952ef351cf
pharaouk <pharaouk@gmail.com>
2023-09-20 02:44:30 +0000

    Docker fixes

--- setup_moe.sh
+++ setup_moe.sh
@@ -2,5 +2,5 @@ pip install -r requirements.txt
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 apt-get install git-lfs
 git lfs install
-git lfs clone https://huggingface.co/HydraLM/gating_v3
+#git lfs clone https://huggingface.co/HydraLM/gating_v3
 
