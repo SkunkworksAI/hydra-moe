283739b89f097592df472371afd27006b2538605
pharaouk <pharaouk@gmail.com>
2023-09-21 23:39:25 +0000

    (artem) fix setup script

--- setup_moe.sh
+++ setup_moe.sh
@@ -2,5 +2,4 @@ pip install -r requirements.txt
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 apt-get install git-lfs
 git lfs install
-git lfs clone https://huggingface.co/HydraLM/router_files
-
+git lfs clone https://huggingface.co/HydraLM/gating_v2 router_files
