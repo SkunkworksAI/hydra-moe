44941221ca10b44c673490dc238ff561b991e4b1
alpayariyak <aariyak@wpi.edu>
2023-09-22 03:10:41 -0400

    Combining utils in preparation for alpha merge

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from model_utils import AttributeDict
+from utils import AttributeDict
 import argparse
 import os
 import subprocess
--- moe.py
+++ moe.py
@@ -1,8 +1,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 from args import *
-from model_utils import *
-from moe_utils import *
+from utils import *
 from collections import defaultdict
 import copy
 import json
