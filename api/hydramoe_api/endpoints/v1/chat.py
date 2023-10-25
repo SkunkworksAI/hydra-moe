fe7986e0262bba2b82b57c2e05661e7b9d9f422c
jphillips <josh.phillips@fearnworks.com>
2023-10-25 12:58:10 -0500

    Add api notebook

--- api/hydramoe_api/endpoints/v1/chat.py
+++ api/hydramoe_api/endpoints/v1/chat.py
@@ -10,12 +10,11 @@ from hydramoe_api import schemas
 
 router = APIRouter()
 
-# Update your FastAPI route
 @router.post("/chat", status_code=200)
 async def chat_endpoint(request: schemas.ChatRequest):
     chat_service = ChatService()
     stream_response = chat_service.submit_query(request)
-    return StreamingResponse(stream_response, media_type="text/event-stream")
+    return StreamingResponse(stream_response, media_type="text/plain")
 
 def fake_data_streamer():
     for i in range(10):
